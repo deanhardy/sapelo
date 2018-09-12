21546abd55981ba77a55b63f86432d2e083a1446
Dean Hardy <deanhardy@gmail.com>
2018-09-12 18:14:46 -0400

    updating events

--- scripts/population_sapelo.R
+++ scripts/population_sapelo.R
@@ -5,24 +5,27 @@ rm(list=ls())
 library(tidyverse)
 library(lubridate)
 
+## define data directory
+datadir <- 'C:/Users/dhardy/Dropbox/r_data/sapelo'
+
 ## import data
-pop <- read.csv("data/population/population_sapelo.csv", stringsAsFactors = F) %>%
+pop <- read.csv(file.path(datadir, "population/population_sapelo.csv"), stringsAsFactors = F) %>%
   mutate(date = as.Date(date, "%m/%d/%Y"), unknown = as.numeric(total), black = as.numeric(black), 
          white = as.numeric(white)) %>%
   select(date, unknown, white, black) %>%
   gather(key = 'race', value = 'population', 2:4)
   # mutate(type = factor(race, levels = c('white', 'black', 'unknown')))
 
-evt <- read.csv('data/population/events.csv', stringsAsFactors = F) %>%
+evt <- read.csv(file.path(datadir, 'population/events.csv'), stringsAsFactors = F) %>%
   mutate(date = as.Date(date, '%m/%d/%Y'))
 
 fig <- ggplot() +
   geom_col(aes(y = population, x = date, fill = race), filter(pop, race == 'unknown'), width = 500) + 
   geom_col(aes(y = population, x = date, fill = factor(race, levels = c('white', 'black'))), 
            filter(pop, race != 'unknown'), width = 500) + 
-  geom_segment(data = evt, aes(y = 0, yend = seq(950, 150, -50), x = date, xend = date), 
+  geom_segment(data = evt, aes(y = 0, yend = seq(950, 150, -40), x = date, xend = date), 
                linetype = 'dashed', size = 0.3) +
-  geom_text(data = evt, aes(x = date, y = seq(950, 150, -50), label = event), 
+  geom_text(data = evt, aes(x = date, y = seq(950, 150, -40), label = event), 
              hjust = -0.01, size = 2.5) + 
   scale_x_date(name = "Year", date_breaks = "20 year", 
                limits = as.Date(c('1860-01-01', '2020-01-01')),
@@ -44,7 +47,7 @@ fig <- ggplot() +
 # expand_limits(x = as.Date('1850-01-01'))
 fig 
 
-tiff('figures/population_sapelo.tif', compression = 'lzw', unit = 'in', height = 5,
+tiff(file.path(datadir, 'figures/population_sapelo.tif'), compression = 'lzw', unit = 'in', height = 5,
      width = 5, res = 300)
 fig
 dev.off()
