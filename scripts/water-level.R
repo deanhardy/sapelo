4be514d5ec17b3abee35bd6dd0fc73cf8545ec83
Dean Hardy <deanhardy@gmail.com>
2019-01-16 11:06:02 -0500

    converted to MLLW

--- scripts/water-level.R
+++ scripts/water-level.R
@@ -6,6 +6,7 @@ library(lubridate)
 ## define data directory
 datadir <- 'C:/Users/dhardy/Dropbox/r_data/sapelo'
 
+## import data--- site 02 (aka "snagtree") MLLW elevation in meters is 1.9678506744 & in feet is 5.456203
 df <- read.delim(file.path(datadir, 'water-level/site02-181012-181109.csv'), header = TRUE, skip = 1,
                  stringsAsFactors = FALSE)[-1] %>%
   rename(water_height_m = Water.Level..meters..LGR.S.N..20441313.,
@@ -16,12 +17,12 @@ df <- read.delim(file.path(datadir, 'water-level/site02-181012-181109.csv'), hea
   slice(., 1:(n()-1)) ## removes last row because erroneous reading 
 
 fig <- ggplot(df) + 
-  geom_line(aes(date_time_gmt, water_height_m * 3.28084)) + 
-  geom_line(aes(date_time_gmt, water_temp_f/25), lty = "dotted", color = 'black') + 
+  geom_line(aes(date_time_gmt, water_height_m * 3.28084 + 5.456203)) + ## convert to feet then add MLLW base elevation
+  geom_line(aes(date_time_gmt, water_temp_f/10), lty = "dotted", color = 'black') + 
   scale_x_datetime(name = 'Date (Year 2018)', date_breaks = '4 day', date_labels = '%m/%d') + 
-  scale_y_continuous(name = 'Local Water Height (feet)',
-                     sec.axis = sec_axis(~. * 25, 
-                                         name = expression(paste('Temperature (',degree,'F)')))) +
+  scale_y_continuous(name = 'Height in feet (MLLW)',
+                     sec.axis = sec_axis(~. * 10, 
+                                         name = expression(paste('Water Temperature (',degree,'F)')))) +
   theme(axis.title = element_text(size = 12),
         axis.text = element_text(color = "black", size = 12),
         axis.ticks.length = unit(-0.2, 'cm'),
@@ -43,6 +44,6 @@ fig <- ggplot(df) +
 fig
 
 tiff(file.path(datadir, 'figures/wl_site02.tif'), res = 300, compression = 'lzw', units = 'in', 
-     height = 4, width = 7)
+     height = 3, width = 6.5)
 fig
 dev.off()
