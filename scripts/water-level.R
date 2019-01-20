61873e222ab98edfd9dba71235d973021f455a9c
Dean Hardy <deanhardy@gmail.com>
2019-01-20 17:43:44 -0500

    join data from two downloads of same logger

--- scripts/water-level.R
+++ scripts/water-level.R
@@ -9,14 +9,24 @@ datadir <- 'C:/Users/dhardy/Dropbox/r_data/sapelo'
 
 ## import data--- site 02 (aka "snagtree") MLLW elevation in meters is 1.9678506744 & in feet is 5.456203
 df <- read.csv(file.path(datadir, 'water-level/hobo-data/site02-181013-181109.csv'), 
-                 header = TRUE, skip = 1,
-                 stringsAsFactors = FALSE)[-1] %>%
-  rename(water_height_m = Water.Level..meters..LGR.S.N..20441313.,
-         water_temp_f = Temp..Â.F..LGR.S.N..20441313..SEN.S.N..20441313.,
-         abs_pres_psi = Abs.Pres..psi..LGR.S.N..20441313..SEN.S.N..20441313.,
-         date_time_gmt = Date.Time..GMT.00.00) %>%
+               header = TRUE, skip = 1,
+               stringsAsFactors = FALSE)[-1]
+colnames(df) <- c('date_time_gmt', 'abs_pres_psi', 'water_temp_f', 'water_height_m')
+
+df <- df %>%
   mutate(date_time_gmt = mdy_hms(date_time_gmt)) %>%
-  slice(., 1:(n()-1)) ## removes last row because erroneous reading 
+  slice(., 2:(n()-1)) ## removes first and last readings
+
+df2 <- read.csv(file.path(datadir, 'water-level/hobo-data/site02-181109-190118.csv'), 
+                header = TRUE, skip = 1,
+                stringsAsFactors = FALSE)[-1]
+colnames(df2) <- c('date_time_gmt', 'abs_pres_psi', 'water_temp_f', 'water_height_m')
+
+df2 <- df2 %>%
+  mutate(date_time_gmt = mdy_hms(date_time_gmt)) %>%
+  slice(., 2:(n()-1)) ## removes first and last readings
+
+df3 <- rbind(df, df2)
 
 nerr <- read.csv(file.path(datadir, 'water-level/nerr-data/lowerduplin-realtime-jan18-nov18.csv'),
                  header = TRUE, stringsAsFactors = FALSE, skip = 2) %>%
@@ -25,16 +35,20 @@ nerr <- read.csv(file.path(datadir, 'water-level/nerr-data/lowerduplin-realtime-
          Depth = as.numeric(Depth)) %>%
   filter(date_time_gmt >= first(df$date_time_gmt) & date_time_gmt <= last(df$date_time_gmt))
 
-fig <- ggplot(df) + 
+ot <- read.delim(file.path(datadir, 'water-level/nerr-date/OldTower-tide-predictions.txt'),
+                 sep = '\t', header = TRUE, skip = 19,
+                 stringsAsFactors = FALSE)
+
+fig <- ggplot(df3) + 
   geom_line(aes(date_time_gmt, water_height_m * 3.28084 + 5.456203)) + ## convert to feet then add MLLW base elevation
   geom_line(aes(date_time_gmt, water_temp_f/10), lty = 'dotted', color = 'black') + 
   # geom_line(aes(date_time_gmt, Depth * 3.28084), data = nerr) + 
-  scale_x_datetime(name = 'Date (Year 2018)', date_breaks = '4 day', date_labels = '%d') + 
+  scale_x_datetime(name = 'Date (Year 2018)', date_breaks = '4 day', date_labels = '%m/%d') + 
   scale_y_continuous(name = 'Height in feet (MLLW)',
                      sec.axis = sec_axis(~. * 10, 
                                          name = expression(paste('Water Temperature (',degree,'F)')))) +
-  theme(axis.title = element_text(size = 12),
-        axis.text = element_text(color = "black", size = 12),
+  theme(axis.title = element_text(size = 8),
+        axis.text = element_text(color = "black", size = 8),
         axis.ticks.length = unit(-0.2, 'cm'),
         axis.ticks = element_line(color = 'black'),
         axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm")), 
@@ -46,14 +60,14 @@ fig <- ggplot(df) +
         panel.grid.major.x = element_line('grey', size = 0.5, linetype = "dotted"),
         plot.margin = margin(0.5,0.5,0.5,0.5, 'cm'),
         legend.position = c(10/15,3),
-        legend.text = element_text(size = 12),
-        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 8),
+        legend.title = element_text(size = 8),
         legend.key = element_blank(),
         legend.box.background = element_rect(color = 'black'))  
 #  labs(caption = "Temperature is dotted line.")
 fig
 
 tiff(file.path(datadir, 'figures/wl_site02.tif'), res = 300, compression = 'lzw', units = 'in', 
-     height = 3, width = 6.5)
+     height = 8.5, width = 11)
 fig
 dev.off()
