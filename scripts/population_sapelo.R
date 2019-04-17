6b4ef970037bf2c814ce4daa2c9ac9e484129281
Dean Hardy <deanhardy@gmail.com>
2019-04-17 14:56:24 -0400

    tweaked date_breaks on x-axis

--- scripts/population_sapelo.R
+++ scripts/population_sapelo.R
@@ -17,28 +17,37 @@ pop <- read.csv(file.path(datadir, "population/population_sapelo.csv"), stringsA
 evt <- read.csv(file.path(datadir, 'population/events.csv'), stringsAsFactors = F, skip = 2) %>%
   mutate(date = as.Date(date, '%m/%d/%Y'))
 
+## from https://stackoverflow.com/questions/14759676/specification-of-first-and-last-tick-marks-with-scale-x-date
+break.vec <- c(as.Date('1860-01-01'), seq(from = as.Date("1860-01-01"), to = as.Date("2020-01-01"),
+                   by = "10 years"), as.Date('2020-01-01'))
+break.date <- as.Date('1855-01-01')
+
 fig <- ggplot() +
-  geom_col(aes(y = population, x = date, fill = race), filter(pop, race == 'unknown'), width = 300) + 
+  geom_col(aes(y = population, x = date, fill = race), 
+           filter(pop, race == 'unknown', date >= break.date), width = 300) + 
   geom_col(aes(y = population, x = date, fill = factor(race, levels = c('white', 'black'))), 
-           filter(pop, race != 'unknown'), width = 300) + 
-  geom_segment(aes(y = 0, yend = seq(990, 150, -(990-150)/(nrow(filter(evt, date >= '1860-01-01'))-1)), 
+           filter(pop, race != 'unknown', date >= break.date), width = 300) + 
+  geom_segment(aes(y = 0, yend = seq(990, 150, -(990-150)/(nrow(filter(evt, date >= break.date))-1)), 
                    x = date, xend = date),
-               filter(evt, date >= '1860-01-01'), 
+               filter(evt, date >= break.date), 
                linetype = 'dashed', size = 0.3) +
-  geom_text(aes(x = date, y = seq(990, 150, -(990-150)/(nrow(filter(evt, date >= '1860-01-01'))-1)), 
+  geom_text(aes(x = date, y = seq(990, 150, -(990-150)/(nrow(filter(evt, date >= break.date))-1)), 
                 label = paste(event, ' ', '(', source, ')', sep = '')),
-            filter(evt, date >= '1860-01-01'), 
+            filter(evt, date >= break.date), 
             hjust = -0.01, size = 2.5) +
-  scale_x_date(name = "Year", date_breaks = "10 years", date_minor_breaks = '10 years',
-               limits = as.Date(c('1860-01-01', '2020-01-01')),
-               date_labels = "%Y",
-               expand = c(0,0)) + 
+  scale_x_date(name = "Year", 
+               breaks = break.vec,
+               # date_breaks = "10 years",
+               # limits = as.Date(c('1860-01-01', '2020-01-01')),
+               expand = c(0.03,0),
+               date_labels = "%Y") + 
   scale_y_continuous(name = "Population",
                      breaks = seq(0,1000, 100),
                      limits = c(0,1000), expand = c(0,0)) +
   scale_fill_manual(name = 'Race', 
                     labels = c('Black', 'Other', 'White'),
                     values = c(black = 'black', unknown = 'grey50', white = 'grey')) +
+# expand_limits(x = as.Date('1850-01-01')) + 
   theme(axis.line = element_line(color = 'black'),
         panel.background = element_rect(fill = FALSE),
         panel.grid = element_blank(),
@@ -46,7 +55,6 @@ fig <- ggplot() +
         plot.margin = margin(1,1,0.5,0.5, 'cm'),
         legend.position = c(0.9,0.9),
         legend.text = element_text(size = 8))
-# expand_limits(x = as.Date('1850-01-01'))
 fig 
 
 tiff(file.path(datadir, 'figures/population_sapelo_revised.tif'), compression = 'lzw', unit = 'in', height = 5,
