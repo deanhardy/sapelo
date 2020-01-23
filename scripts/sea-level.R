19a7ba26077e54c9652cb36d2fc27210d744b9eb
“Dean <deanhardy@gmail.com>
2020-01-23 12:35:50 -0500

    working on writing for loop or something similar to grab consecutive 10-yr periods

--- scripts/sea-level.R
+++ scripts/sea-level.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 
-library(rnoaa)
+library(rnoaa) ## package info https://cran.r-project.org/web/packages/rnoaa/rnoaa.pdf
 library(tidyverse)
 
 ## define data directory
@@ -11,10 +11,12 @@ test <- read.csv("https://tidesandcurrents.noaa.gov/sltrends/data/USAverageSeaso
                  stringsAsFactors = FALSE)
 
 ## define stations of interest
-STATIONS <- c(8670870, 8720030)
+STATIONS <- c(8670870, 8720030) ## Fort Pulaski and Fernandina
 DATUM <- 'MSL'
+# TIME <- c(3653, 3653*2)
 
-l <- 
+## grab most recent decade of data
+T1 <- 
   lapply(STATIONS, function(z) {
     coops_search(begin_date = as.character(as.Date(Sys.Date()-3653, format = '%Y%m%d')) %>% 
                    gsub('-', '', .) %>%
@@ -29,21 +31,43 @@ l <-
                  time_zone = 'GMT')
 }$data)
 
-df <- l[[1]] %>%
+## grab second most recent decade of data
+T2 <- 
+  lapply(STATIONS, function(z) {
+    coops_search(begin_date = as.character(as.Date(Sys.Date()-3653*2, format = '%Y%m%d')) %>% 
+                   gsub('-', '', .) %>%
+                   as.numeric(),
+                 end_date = as.character(as.Date(Sys.Date()-3653, format = '%Y%m%d')) %>% 
+                   gsub('-', '', .) %>%
+                   as.numeric(),
+                 station_name = z,
+                 product = 'monthly_mean', 
+                 datum = DATUM, 
+                 units = 'metric', 
+                 time_zone = 'GMT')
+  }$data)
+
+T1_1 <- T1[[1]] %>%
   mutate(station = as.character(STATIONS[1]))
-df2 <- l[[2]] %>%
+T1_2 <- T1[[2]] %>%
+  mutate(station = as.character(STATIONS[2]))
+T2_1 <- T2[[1]] %>%
+  mutate(station = as.character(STATIONS[1]))
+T2_2 <- T2[[2]] %>%
   mutate(station = as.character(STATIONS[2]))
 
-df3 <- rbind(df, df2)
+df1 <- rbind(T1_1, T1_2)
+df2 <- rbind(T2_1, T2_2)
+df <- rbind(df1, df2)
 
-df4 <- df3 %>%
+dat <- df %>%
   mutate(yrmo = paste(year, month, sep = '-')) %>%
   mutate(date = as.Date(paste(yrmo, '-01', sep = '')))
 
-fig <- ggplot(filter(df4, station == STATIONS[1]), aes(x = date, y = MSL, color = station)) +
-  geom_line() +
-  geom_point() + 
-  #geom_smooth() + 
+fig <- ggplot(filter(dat, station == STATIONS[1]), aes(x = date, y = MSL, color = station)) +
+  #geom_line() +
+  geom_smooth(method = 'loess') + 
+  geom_point(size = 0.5) + 
   scale_y_continuous(name = paste('Datum', DATUM, '(m)'),
                      breaks = round(seq(-0.2, 0.4, by = 0.1), 2),
                      minor_breaks = seq(-0.2, 0.4, by = 0.05)) + 
@@ -51,8 +75,9 @@ fig <- ggplot(filter(df4, station == STATIONS[1]), aes(x = date, y = MSL, color 
                date_breaks = '6 months', 
                date_minor_breaks = '1 month',
                date_labels = '%b-%y') + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        legend.position = 'none')
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6),
+        legend.position = 'none') + 
+  ggtitle(paste("Monthly Mean Sea Level, NOAA Tide Station ID:", STATIONS[1]))
 fig
 
 tiff(file.path(datadir, '/figures/sea-level.tif'), units = 'in', 
