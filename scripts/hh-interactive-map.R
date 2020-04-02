846d58eae604e185ae6d8be0b63885a9c97c3d35
“Dean <deanhardy@gmail.com>
2020-04-02 16:26:17 -0400

    working on adding raster

--- scripts/hh-interactive-map.R
+++ scripts/hh-interactive-map.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(sf)
 library(leaflet)
 library(leaflet.extras)
+library(raster)
 # devtools::install_github("statnmap/HatchedPolygons")
 
 ## define data directory
@@ -109,6 +110,10 @@ forsale <- df2 %>% filter(parcel_id %in% c('0101A 0004003', '0101A 0019003', '01
   st_centroid() ## last one deduced from legal desc online, parcel id wrong
 
 weblink <- '<a href=https://www.zillow.com/sapelo-island-ga/?searchQueryState={%22pagination%22:{},%22usersSearchTerm%22:%22Sapelo%20Island,%20GA%22,%22mapBounds%22:{%22west%22:-81.44322331103515,%22east%22:-81.04153568896484,%22south%22:31.34861701591248,%22north%22:31.562411869538245},%22mapZoom%22:12,%22savedSearchEnrollmentId%22:%22X1-SS0h1ut76czy191000000000_4hjdl%22,%22regionSelection%22:[{%22regionId%22:54314,%22regionType%22:6}],%22isMapVisible%22:true,%22filterState%22:{},%22isListVisible%22:true}>Zillow Sales Data</a>'
+
+## import 1938 map
+dem <- raster(x = file.path(datadir, "spatial-data/sapelo_hog_hammock/sapelo_hog_hammock.tif"))
+GDALinfo(file.path(datadir, "spatial-data/sapelo_hog_hammock/sapelo_hog_hammock.tif"))
 
 ## define map variables
 clr4 <- c('black', 'grey60', 'orange', 'white')
