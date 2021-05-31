489bbbcb9c6ee72dc12d9feafb541578c494ec7e
“Dean <deanhardy@gmail.com>
2021-05-31 16:02:58 -0400

    ...

--- scripts/inundation-figure.R
+++ scripts/inundation-figure.R
@@ -16,6 +16,7 @@ datadir <- '/Users/dhardy/Dropbox/r_data/sapelo'
 df <- st_read(file.path(datadir, 'spatial-data/parcel_data_export/parcel_data.shp'), stringsAsFactors = F) %>%
   st_transform(4326) %>%
   mutate(owner = ifelse(is.na(owner), 'unknown', owner)) %>%
+  mutate(own3cat = ifelse(own3cat == 'Non-traditional', 'Non-Descendant', own3cat))
   filter(own4cat != 'County' & gis_acres != 'NA')
 
 ## filter out just companies
@@ -52,50 +53,50 @@ clr.own <- c('black', 'grey60', 'orange')
 clr.ind <- c('#00a9e6', '#004c73')
 leafIcon <- tmap_icons("http://leafletjs.com/examples/custom-icons/leaf-green.png")
 
-map.own.ind <- 
-  tm_shape(df) + 
-    tm_fill('own3cat', palette = clr.own, 
-            title = 'Owner Category') + 
-  tm_shape(df) + tm_borders(lwd = 0.5) + 
-  tm_shape(comp) + tm_borders('yellow') + 
-  tm_shape(ag) + tm_fill('green') + 
-  # tm_shape(inund,
-  #          raster.downsample = FALSE) + 
-  #   tm_raster(alpha = 0.5,
-  #             n = 4,
-  #             as.count = FALSE) + 
-  tm_shape(inund) + 
-    tm_fill('prb_smplfy', alpha = 0.5, palette = clr.ind,
-            title = 'Inundation Probability') + 
-  tm_shape(tidal, raster.downsample = FALSE) + 
-    tm_raster(title = '', alpha = 1, palette = '#89cd66', legend.show = FALSE) + 
-  tm_shape(ag_cntr) + 
-    tm_symbols(shape = leafIcon, border.lwd = NA,
-             just = c(0.4, 2),
-             size = 0.5) + 
-  tm_shape(hobo) +
-    tm_markers(shape = marker_icon(),
-               just = c(0,2),
-             size = 0.1) + 
-  tm_layout(frame = FALSE,
-            legend.text.size = 0.6,
-            legend.title.size = 0.7) +
-  tm_add_legend(type = 'fill',
-                labels = 'MHHW Extent',
-                col = '#89cd66',
-                border.lwd = 0) 
-  # tm_add_legend(type = 'symbol',
-  #               label = 'Agricultural Plot',
-  #               shape = leafIcon) + 
-  # tm_add_legend(type = 'symbol',
-  #               shape = marker_icon(),
-  #               label = 'Water Level Logger')
-map.own.ind
-
-tiff(file.path(datadir, 'figures/doubleD-fig.tiff'), units = 'in', width = 3.5, height = 3.5, 
-     res = 300, compression = 'lzw')
-map.own.ind
-dev.off()
+# map.own.ind <- 
+#   tm_shape(df) + 
+#     tm_fill('own3cat', palette = clr.own, 
+#             title = 'Owner Category') + 
+#   tm_shape(df) + tm_borders(lwd = 0.5) + 
+#   tm_shape(comp) + tm_borders('yellow') + 
+#   tm_shape(ag) + tm_fill('green') + 
+#   # tm_shape(inund,
+#   #          raster.downsample = FALSE) + 
+#   #   tm_raster(alpha = 0.5,
+#   #             n = 4,
+#   #             as.count = FALSE) + 
+#   tm_shape(inund) + 
+#     tm_fill('prb_smplfy', alpha = 0.5, palette = clr.ind,
+#             title = 'Inundation Probability') + 
+#   tm_shape(tidal, raster.downsample = FALSE) + 
+#     tm_raster(title = '', alpha = 1, palette = '#89cd66', legend.show = FALSE) + 
+#   tm_shape(ag_cntr) + 
+#     tm_symbols(shape = leafIcon, border.lwd = NA,
+#              just = c(0.4, 2),
+#              size = 0.5) + 
+#   tm_shape(hobo) +
+#     tm_markers(shape = marker_icon(),
+#                just = c(0,2),
+#              size = 0.1) + 
+#   tm_layout(frame = FALSE,
+#             legend.text.size = 0.6,
+#             legend.title.size = 0.7) +
+#   tm_add_legend(type = 'fill',
+#                 labels = 'MHHW Extent',
+#                 col = '#89cd66',
+#                 border.lwd = 0) 
+#   # tm_add_legend(type = 'symbol',
+#   #               label = 'Agricultural Plot',
+#   #               shape = leafIcon) + 
+#   # tm_add_legend(type = 'symbol',
+#   #               shape = marker_icon(),
+#   #               label = 'Water Level Logger')
+# map.own.ind
+# 
+# tiff(file.path(datadir, 'figures/doubleD-fig.tiff'), units = 'in', width = 3.5, height = 3.5, 
+#      res = 300, compression = 'lzw')
+# map.own.ind
+# dev.off()
 
 
 ## separate inundation map
@@ -162,7 +163,7 @@ map.own
 
 library(grid)
 library(gridExtra)
-tiff(file.path(datadir, 'figures/doubleD-fig-option1.tiff'), units = 'in', width = 7, height = 3.5, 
+tiff(file.path(datadir, 'figures/blockade-fig.tiff'), units = 'in', width = 7, height = 3.5, 
      res = 150, compression = 'lzw')
 tmap_arrange(map.ind, map.own, widths = c(0.5, 0.5))
 dev.off()
