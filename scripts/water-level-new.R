1c6b2724076702e35b3eb34f416df46ee62da12e
Dean Hardy <deanhardy@gmail.com>
2022-04-26 00:31:18 +0000

    date changes

--- scripts/water-level-new.R
+++ scripts/water-level-new.R
@@ -13,8 +13,8 @@ datadir <- '/Users/dhardy/Dropbox/r_data/sapelo/water-level/'
 # level.var <- c('water_depth_m')
 
 # set dates for interval graphs
-int.date1 <- as.Date('2021-11-02') 
-int.date2 <- as.Date('2022-02-28')
+int.date1 <- as.Date('2021-12-01') 
+int.date2 <- as.Date('2022-01-31')
 
 # set dates for daily high tide graphs
 ht.date1 <- as.Date('2018-10-01') 
