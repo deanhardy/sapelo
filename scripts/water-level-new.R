c333b50018351501a12dda1b47b1bafc1c174267
Dean Hardy <deanhardy@gmail.com>
2023-05-17 02:02:57 +0000

    fixed some errors

--- scripts/water-level-new.R
+++ scripts/water-level-new.R
@@ -228,23 +228,36 @@ tidal3 <- tidal2 %>%
 date1 <- as.Date('2018-10-01') 
 date2 <- as.Date('2023-04-25') 
   
-tidal4 <- tidal3 %>%
+tidal3.1 <- tidal3 %>%
   mutate(type = ifelse(site %in% c('Site-13', 'Site-11', 'Site-09', 'Site-05', 'Site-19', 'Site-14', 'Site-15', 'Site-18', 'Site-20', 'Site-23', 'Site-24'), 'ditch', 
                        ifelse(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12', 'Site-16'), 'creek', site))) %>% ## ditches sites
   # filter(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12')) %>% ## marsh sites
   mutate(transect = ifelse(site %in% c('Site-06', 'Site-12', 'Site-19', 'Site-13', 'Site-18'), 'T1', 
-                           ifelse(site %in% c('Site-02', 'Site-03', 'Site-05','Site-11', 'Site-23'), 'T2',
-                                  ifelse(site %in% c('Site-07', 'Site-09'), 'T3N', 
+                           ifelse(site %in% c('Site-02', 'Site-03', 'Site-05','Site-11', 'Site-23'), 'T3',
+                                  ifelse(site %in% c('Site-07', 'Site-09', 'Site-24'), 'T4', 
                                          ifelse(site %in% c('Site-16', 'Site-15'), "T5",
-                                                ifelse(site %in% c('Site-20'), 'T4',
-                                                       ifelse(site %in% c('Site-07', 'Site-24'), 'T3S', site))))))) 
+                                                ifelse(site %in% c('Site-20'), 'T2', site))))))
 
 ## working on renaming sites to be more logical related to transects; site-05 is a branch of T2
-# tidal5 <- tidal4 %>%
-#   mutate(site_new = if_else(site = 'Site-02', 'T2-01',
-#                             if_else()))
-  
-sites <- tidal4 %>%
+tidal3.2 <- tidal3.1 %>%
+  mutate(site_new = if_else(site == 'Site-06', 'T1-01',
+                            if_else(site == 'Site-12', 'T1-02', 
+                                    if_else(site == 'Site-19', 'T1-03',
+                                            if_else(site == 'Site-13', 'T1-04',
+                                                    if_else(site == 'Site-18', 'T1-05',
+                                                            if_else(site == 'Site-20', 'T2-01', 
+                                                                    if_else(site == 'Site-02', 'T3-01',
+                                                                            if_else(site == 'Site-03', 'T3-02',
+                                                                                    if_else(site =='Site-05', 'T3-BR-01',
+                                                                                            if_else(site == 'Site-11', 'T3-03',
+                                                                                                    if_else(site == 'Site-23', 'T3-04',
+                                                                                                            if_else(site == 'Site-07', 'T4-01',
+                                                                                                                    if_else(site == 'Site-09', 'T4-02',
+                                                                                                                            if_else(site == 'Site-24', 'T4S-01',
+                                                                                                                                    if_else(site == 'Site-16', 'T5-01',
+                                                                                                                                            if_else(site == 'Site-15', 'T5-02', site)))))))))))))))))
+
+sites <- tidal3.2 %>%
   filter(date_time_gmt >= date1 & date_time_gmt <= date2) %>%
   select(site, type, transect, date_time_gmt, water_depth_m, water_level_navd88, water_temp_c)
 
