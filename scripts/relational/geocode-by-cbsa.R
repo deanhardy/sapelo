6b56e6534af81295a41ec7dc43a75976aa4ba3a2
“Dean <deanhardy@gmail.com>
2024-01-12 13:00:53 -0500

    small tweaks

--- scripts/relational/geocode-by-cbsa.R
+++ scripts/relational/geocode-by-cbsa.R
@@ -23,10 +23,10 @@ cbsa <- get_acs(geography = 'cbsa',
 cbsa.df <- st_drop_geometry(cbsa) %>%
   rename(cbsa.name = NAME)
 
-cbsa %>%
-  ggplot(aes(fill = estimate)) + 
-  geom_sf(color = NA) + 
-  scale_fill_viridis_c(option = "magma") 
+# cbsa %>%
+#   ggplot(aes(fill = estimate)) + 
+#   geom_sf(color = NA) + 
+#   scale_fill_viridis_c(option = "magma") 
 
 ## import geocoded data
 geo <- st_read(file.path(datadir, 'spatial-data/geocoded/tax_geocode.GEOJSON'), stringsAsFactors = F) %>%
@@ -54,7 +54,7 @@ geo.fltr.cbsa <- geo.cbsa %>%
 ## create sf vector for sapelo point
 sap <- data.frame(x = -81.26206, y = 31.42655)
 sap2 <- st_as_sf(sap, coords = c('x', 'y'), crs = 4269) %>%
-  mutate(year = 'NA', category = 'descendant', cbsa.name = 'sapelo', count = 1)
+  mutate(year = 'NA', category = 'sapelo', cbsa.name = 'sapelo', count = 1)
 
 r.all <- rbind(geo.fltr.cbsa, sap2)
 
@@ -70,14 +70,16 @@ geo.cbsa2 <- cbind(geo.cbsa, geo.dist)
 
 mn.dist <- geo.cbsa2 %>%
   group_by(year, category) %>%
-  summarise(mn.dist = mean(sap.dist)) %>%
+  summarise(mn.dist = mean(sap.dist), max.dist = max(sap.dist), min.dist = min(sap.dist)) %>%
   rename(ownership = category)
+
+boxplot(sap.dist ~ category, data = geo.cbsa2, outline = F, notch = T)
 
 fig.dist <- mn.dist %>%
   filter(year < 2023, ownership != 'unknown') %>%
   ggplot(aes(year, mn.dist, color = ownership)) +
   geom_point() + 
-  scale_y_continuous(name = "Nonlocal Tax Addresses (%)",
+  scale_y_continuous(name = "Nonlocal Tax Addresses (miles)",
                      breaks = seq(0,320, 20),
                      limits = c(0,320)) + 
   scale_x_continuous(breaks = seq(1999, 2022, 3), minor_breaks = seq(1999,2022,1)) + 
@@ -147,35 +149,43 @@ points_to_lines <- function(data, ids, names, order_matters = TRUE) {
 usa <- st_as_sf(maps::map("state", fill=TRUE, plot =FALSE))
 
 YR <- seq(1999, 2022, 1)
-# YR <- c('2022', '2010')
+# YR <- c('2022')
 
 for (z in seq_along(YR)) {
   
-r.df <- filter(geo.fltr.cbsa, year == YR[z])
+r.df <- filter(geo.fltr.cbsa, year == YR[z]) %>%
+  st_transform(5070)
 
 ## generate relational lines between sapelo and tax address
 r.l <- r.all %>%
   filter(year %in% c(YR[z], 'NA')) %>%
   points_to_lines(., ids = 'cbsa.name', names = 'category', order_matters = F) %>%
   filter(start == 'sapelo') %>%
-  mutate(Year = YR[z])
+  mutate(Year = YR[z]) %>%
+  st_transform(5070)
 
 fig <- ggplot() +
   geom_sf(data = usa) +   
   geom_sf(color = "#2b2b2b", fill = "white", size=0.125) +
-  # coord_sf(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"),
-  #          datum = NA) +
   ggthemes::theme_map() +
-  geom_sf(data = r.l, color = "black") +
+  geom_sf(data = r.l, color = "black", ) +
   geom_point(
     aes(color = category, size = count, geometry = geometry),
     data = filter(r.df, category != 'unknown'),
     stat = "sf_coordinates",
     alpha = 0.8,
   ) + 
-  scale_size(range = c(1, 10), name="Parcels Per CBSA") + 
-  theme(legend.position = "bottom") + 
-  ggtitle(paste('Year = ', YR[z], '; # of Tax Addresses = ', sum(r.df$count)))
+  coord_sf(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"),
+           datum = NA) +
+  scale_size(breaks = c(10,20,30,40,50), limits = c(0,50), name="Parcels Per CBSA") + 
+  scale_color_discrete(name = 'Ownership', labels = c('Descendant', 'Nontraditional')) +
+  theme(legend.position = c(0.01,0.01)) + 
+  ggtitle(paste0('Year ', YR[z], '; ', sum(r.df$count),  ' Tax Addresses')) + 
+  guides(color = guide_legend(order=1),
+         size = guide_legend(order=2))
+
+# legend help
+# http://www.sthda.com/english/wiki/ggplot2-legend-easy-steps-to-change-the-position-and-the-appearance-of-a-graph-legend-in-r-software
 
 # save plots as .png
 ggsave(fig, file=paste(datadir,
@@ -186,4 +196,5 @@ ggsave(fig, file=paste(datadir,
 ## create animated gif of outputs using ImageMagick in Terminal
 ## change to directory of png files
 ## convert -delay 100 -loop 0 *.png animation.gif
+## https://cran.r-project.org/web/packages/magick/vignettes/intro.html#Animated_Graphics
     
