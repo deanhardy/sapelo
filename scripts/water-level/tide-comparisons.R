a98feabc19b720f3c3901e7a320f56a4f5f674ae
Dean Hardy <deanhardy@gmail.com>
2024-10-22 02:16:12 +0000

    updating tide comp figs

--- scripts/water-level/tide-comparisons.R
+++ scripts/water-level/tide-comparisons.R
@@ -14,8 +14,8 @@ Sys.setenv(TZ='GMT')
 datadir <- '/Users/dhardy/Dropbox/r_data/sapelo/water-level/'
 
 # set dates for transect graphs
-int.date1 <- as.Date('2022-11-04') 
-int.date2 <- as.Date('2022-11-07') 
+int.date1 <- as.Date('2023-11-04') 
+int.date2 <- as.Date('2023-11-07') 
 
 ######################
 ## import & tidy data
@@ -52,25 +52,22 @@ ml <- readNWISuv(siteNumbers = siteNo,
 df2 <- rbind(df, ml)
 
 ## set dates and sites
-df3 <- filter(df2, site_new %in% c('ML', 'T4-01', 'T1-02') & date_time_gmt >= int.date1 & date_time_gmt <= int.date2)
+df3 <- filter(df2, site_new %in% c('ML', 'T4-01', 'T4-02', 'T4-BR-01') & date_time_gmt >= int.date1 & date_time_gmt <= int.date2)
 
 TEXT = 8
 plot <- 
   ggplot(df3)  + 
   geom_line(aes(date_time_gmt, water_level_navd88, color = site_new)) +
-  scale_x_datetime(name = 'Date', date_breaks = '12 hours', date_minor_breaks = '1 hour', date_labels = '%m/%d %H:%M') + 
-  scale_y_continuous(name = 'Water Level (m NAVD88)', breaks = seq(-1.5,1.5,0.25), limits = c(-1.5,1.5), expand = c(0,0)) +
+  scale_x_datetime(name = 'Date', date_breaks = '12 hours', date_minor_breaks = '1 hour', date_labels = '%m/%d/%y %H:%M') + 
+  scale_y_continuous(name = 'Water Level (m NAVD88)', minor_breaks = seq(-1.6,2,0.1), breaks = seq(-1.6,2,0.4), limits = c(-1.6,2), expand = c(0,0)) +
+  labs(color='Site') +
   theme(axis.title = element_text(size = TEXT),
         axis.text = element_text(color = "black", size = TEXT),
         axis.ticks.length = unit(-0.2, 'cm'),
         axis.ticks = element_line(color = 'black'),
-        axis.text.x = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm")), 
+        axis.text.x = element_text(angle = 45, vjust = 1.2, hjust=1, margin=unit(c(0.5,0.5,0.5,0.5), "cm")), 
         axis.text.y = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm")),
         axis.line = element_line(color = 'black'),
-        axis.text.y.right = element_text(margin=unit(c(0.5,0.5,0.5,0.5), "cm"), color = 'blue'),
-        axis.title.y.right = element_text(color = 'blue'),
-        axis.line.y.right = element_line(color = "blue"), 
-        axis.ticks.y.right = element_line(color = "blue"),
         panel.background = element_rect(fill = FALSE, color = 'black'),
         panel.grid = element_blank(),
         panel.grid.major.x = element_line('grey30', size = 0.5, linetype = "dotted"),
@@ -84,6 +81,6 @@ plot <-
         plot.title = element_text(size = TEXT, face = "bold"))
 plot
 
-tiff(paste0(datadir, 'figures/tide-comps.tiff'), unit = 'in', height = 4, width = 6.5, res = 300)
+tiff(paste0(datadir, 'figures/tide-comps-t4.tiff'), unit = 'in', height = 4, width = 6.5, res = 300)
 plot
 dev.off()
