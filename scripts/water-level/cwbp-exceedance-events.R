71203c802acd0da6d76a03d4627567bef89fb1ee
“Dean <deanhardy@gmail.com>
2025-01-08 20:12:39 +0000

    checking ML against CWBP sites for extreme water level trends

--- scripts/water-level/cwbp-exceedance-events.R
+++ scripts/water-level/cwbp-exceedance-events.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(lubridate)
 library(data.table)
 library(readxl)
+library(dataRetrieval)
 library("rio")
 Sys.setenv(TZ='GMT')
 
@@ -18,23 +19,53 @@ datadir <- '/Users/dhardy/Dropbox/r_data/sapelo/water-level/'
 ## import cleaned water level data
 df <- read_csv(paste(datadir, 'wls_data.csv'))[,-1] %>%
   mutate(date_time_gmt = as.POSIXct(date_time_gmt, format = "%Y-%m-%d %H:%M:%S", tz = 'GMT'),
-         date = as.POSIXct(date))
+         date = as.POSIXct(date)) 
 
 ## monthly high water means
 df.mhhw <- df %>%
-  mutate(prd = floor_date(date_time_gmt, "day")) %>%
-  group_by(transect, site_new, prd) %>%
+  mutate(date = floor_date(date_time_gmt, "day")) %>%
+  group_by(transect, site_new, date) %>%
   summarise(max = max(water_level_navd88)) %>%
-  mutate(mhhw_max_ft = max*3.28084+3.9566929) ## VDATUM for Cabretta Creek mouth near HH community
+  mutate(mhhw_max_ft = max*3.28084+3.9566929) %>% ## VDATUM for Cabretta Creek mouth near HH community
+  rename(site = site_new)
+
+########################################################################################################
+## adding automated download process for USGS data at Meridian to compare to CWBP 
+########################################################################################################
+
+# Hudson River, Meridian, GA
+siteNo <- "022035975"
+pCode <- "00065" ## gage height data
+statCode <- "00021" ## tidal high-high values
+start.date <- "2019-01-01" ## earliest available date
+end.date <- "2024-01-01"
+
+df2 <- readNWISdv(siteNumbers = siteNo,
+                 parameterCd = pCode,
+                 startDate = start.date,
+                 endDate = end.date,
+                 statCd = statCode) %>%
+  rename(mhhw_max_ft = X_00065_00021,
+         quality = X_00065_00021_cd,
+         date = Date,
+         site = site_no,
+         transect = agency_cd) %>%
+  mutate(type = 'high', max = 0) %>%
+  select(transect, site, date, max, mhhw_max_ft) %>%
+  mutate(mhhw_max_ft = mhhw_max_ft + 4.18)
+
+df2$date <- as.POSIXct(df2$date) ## convert datetime column to correct format
+
+df.mhhw2 <- rbind(df.mhhw, df2)
 
 ############################################
 ## count number of events above flood stage
 ############################################
-df4 <- df.mhhw %>%
-  filter(mhhw_max_ft > (9) & site_new %in% c('T1-02', 'T2-01', 'T3-03', 'T4-01') & prd < '2024-01-01' & prd > '2019-01-01' ) %>% ## flood stage = 10.2 feet
-  mutate(x = floor_date(prd, "year")) %>%
+df4 <- df.mhhw2 %>%
+  filter(mhhw_max_ft > (9.2) & site %in% c('T1-02', 'T3-02', 'T4-01', '022035975') & date < '2024-01-01' & date > '2019-01-01' ) %>%
+  mutate(x = floor_date(date, "year")) %>%
   mutate(x = year(x)) %>%
-  group_by(site_new, x) %>%
+  group_by(site, x) %>%
   summarise(y = n())
 
 ## https://stackoverflow.com/questions/37329074/geom-smooth-and-exponential-fits
@@ -60,7 +91,7 @@ ext <- ggplot(df4, aes(x, y, label = y)) +
         panel.background = element_rect(color = 'grey10', fill = 'white', linewidth = 0.5),
         panel.grid = element_line(color = 'grey90')) +  
   labs(x = 'Year', y = '# Events > 9 ft') + 
-  facet_wrap('site_new')
+  facet_wrap('site')
 ext
 
 # tiff(file.path(datadir, 'figures/cwbp_t1-02action_stage.tiff'), res = 300, unit = 'in',
