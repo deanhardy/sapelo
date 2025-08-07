a13edc018af9baf6e4fa803151c8f044db38f90c
“Dean <deanhardy@gmail.com>
2025-08-07 02:02:51 +0000

    working out re-write using sensor depth instead of water level from reference measurements. need to divide data by logger serials for logger length reference measurement

--- scripts/water-level/wls-data-cleaning2.R
+++ scripts/water-level/wls-data-cleaning2.R
@@ -85,7 +85,7 @@ msmt.A.avgs3 <- left_join(msmt.A.avgs2, msmt.B.avgs)
 #######################################################
 ## import logger sensor depth data files and tidy
 #######################################################
-filz <- list.files(path = file.path(datadir, 'new-logger-data/'),
+filz <- list.files(path = file.path(datadir, 'new-logger-data/hobo_sensor_depth'),
                    pattern= '*.csv',
                    full.names = TRUE,
                    recursive = F)
@@ -111,7 +111,7 @@ filz <- list.files(path = file.path(datadir, 'new-logger-data/'),
 #                        full.names = TRUE,
 #                        recursive = TRUE) 
 ## read test file
-# tf <- read.csv(paste0(datadir, 'new-logger-data/site06_1316_181108-211213.csv'), skip = 1)
+# tf <- read.csv(paste0(datadir, 'new-logger-data/hobo_sensor_depth/t102_0000_181013-250426.csv'), skip = 1)
 
 ## import & tidy hobo water level data
 tidal <- NULL
@@ -126,30 +126,31 @@ for(i in 1:length(filz)) {
       as.POSIXct(date_time_gmt, format = '%m/%d/%y %I:%M:%S %p', tz = 'GMT'),
       as.POSIXct(date_time_gmt, format = '%m/%d/%y %H:%M:%S', tz = 'GMT')),
       date = as.Date(date_time_gmt, format = '%m/%d/%y', tz = 'GMT'),
-      site = str_sub(filz[i], -25,-24),
+      transect = toupper(str_sub(filz[i], -27,-26)),
+      site = toupper(paste0(str_sub(filz[i], -27,-26), '-', str_sub(filz[i], -25,-24))),
       serial = str_sub(filz[i], -22,-19),
       water_temp_c = as.numeric(water_temp_c),
       sensor_depth = as.numeric(sensor_depth),
       temp_max = max(water_temp_c)) %>%
-    mutate(site = paste('Site', site, sep = '-')) %>%
-    mutate(name = if_else(site == 'Site-02', 'Snagtree',
-                          if_else(site == 'Site-03', 'St. Lukes',
-                                  if_else(site == 'Site-05', 'Graball',
-                                          if_else(site == 'Site-06', 'Dani Trap',
-                                                  if_else(site == 'Site-07', 'Cactus Patch',
-                                                          if_else(site == 'Site-09', 'Mr. Tracy',
-                                                                  if_else(site == 'Site-11', 'Library',
-                                                                          if_else(site == 'Site-12', 'Mr. Smith',
-                                                                                  if_else(site == 'Site-13', 'Purple Ribbon',
-                                                                                          if_else(site == 'Site-14', 'Tidal Gate',
-                                                                                                  if_else(site == 'Site-19', 'The Trunk',
-                                                                                                          if_else(site == 'Site-16', 'South Oakdale',
-                                                                                                                  if_else(site == 'Site-18', 'NW Corner',
-                                                                                                                          if_else(site == 'Site-20', 'Walker',
-                                                                                                                                  if_else(site == 'Site-23', 'Hillery',
-                                                                                                                                          if_else(site == 'Site-24', 'Johnson', 
-                                                                                                                                                  if_else(site == 'Site-15', 'Oakdale', site)))))))))))))))))) %>%
-    mutate(sitename = paste(site, name)) %>%
+    # mutate(site = paste('Site', site, sep = '-')) %>%
+    # mutate(name = if_else(site == 'Site-02', 'Snagtree',
+    #                       if_else(site == 'Site-03', 'St. Lukes',
+    #                               if_else(site == 'Site-05', 'Graball',
+    #                                       if_else(site == 'Site-06', 'Dani Trap',
+    #                                               if_else(site == 'Site-07', 'Cactus Patch',
+    #                                                       if_else(site == 'Site-09', 'Mr. Tracy',
+    #                                                               if_else(site == 'Site-11', 'Library',
+    #                                                                       if_else(site == 'Site-12', 'Mr. Smith',
+    #                                                                               if_else(site == 'Site-13', 'Purple Ribbon',
+    #                                                                                       if_else(site == 'Site-14', 'Tidal Gate',
+    #                                                                                               if_else(site == 'Site-19', 'The Trunk',
+    #                                                                                                       if_else(site == 'Site-16', 'South Oakdale',
+    #                                                                                                               if_else(site == 'Site-18', 'NW Corner',
+    #                                                                                                                       if_else(site == 'Site-20', 'Walker',
+    #                                                                                                                               if_else(site == 'Site-23', 'Hillery',
+    #                                                                                                                                       if_else(site == 'Site-24', 'Johnson', 
+    #                                                                                                                                               if_else(site == 'Site-15', 'Oakdale', site)))))))))))))))))) %>%
+    # mutate(sitename = paste(site, name)) %>%
     select(!(abs_pres_psi))
   tidal <- rbind(OUT, tidal)
 }
@@ -157,42 +158,7 @@ for(i in 1:length(filz)) {
 ## select relevant hobo data columns
 tidal.01 <- tidal %>%
   mutate(logger = 'hobo') %>%
-  select(date_time_gmt, water_temp_c, sensor_depth, date, site, name, sitename, logger, serial)
-
-## adding transects with idea that they work north to south then east to west, or clockwise around the community
-tidal3.1 <- tidal.01 %>%
-  mutate(type = ifelse(site %in% c('Site-13', 'Site-11', 'Site-09', 'Site-05', 'Site-19', 'Site-14', 'Site-15', 'Site-18', 'Site-20', 'Site-23', 'Site-24'), 'ditch',
-                       ifelse(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12', 'Site-16'), 'creek', site))) %>% ## ditches sites
-  # filter(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12')) %>% ## marsh sites
-  mutate(transect = ifelse(site %in% c('Site-06', 'Site-12', 'Site-19', 'Site-13', 'Site-18'), 'T1',
-                           ifelse(site %in% c('Site-02', 'Site-03', 'Site-05','Site-11', 'Site-23'), 'T3',
-                                  ifelse(site %in% c('Site-07', 'Site-09', 'Site-24'), 'T4',
-                                         ifelse(site %in% c('Site-16', 'Site-15'), "T5",
-                                                ifelse(site %in% c('Site-20'), 'T2',
-                                                       if_else(site %in% c('Site-14'), 'T6', site)))))))
-
-## renaming sites to be more logical related to transects; site-05 is a branch of T3
-tidal3.2 <- tidal3.1 %>%
-  mutate(site_new = if_else(site == 'Site-06', 'T1-01',
-                            if_else(site == 'Site-12', 'T1-02', 
-                                    if_else(site == 'Site-19', 'T1-03',
-                                            if_else(site == 'Site-13', 'T1-04',
-                                                    if_else(site == 'Site-18', 'T1-05',
-                                                            if_else(site == 'Site-20', 'T2-01', 
-                                                                    if_else(site == 'Site-02', 'T3-01',
-                                                                            if_else(site == 'Site-03', 'T3-02',
-                                                                                    if_else(site =='Site-05', 'T3-BR-01',
-                                                                                            if_else(site == 'Site-11', 'T3-03',
-                                                                                                    if_else(site == 'Site-23', 'T3-04',
-                                                                                                            if_else(site == 'Site-07', 'T4-01',
-                                                                                                                    if_else(site == 'Site-09', 'T4-BR-01',
-                                                                                                                            if_else(site == 'Site-24', 'T4-02',
-                                                                                                                                    if_else(site == 'Site-16', 'T5-01',
-                                                                                                                                            if_else(site == 'Site-15', 'T5-02', 
-                                                                                                                                                    if_else(site == 'Site-14', 'T6-01', site)))))))))))))))))) %>%
-  mutate(sitename_new = paste(site_new, name),
-         site_serial = paste0(site_new, ' (', serial, ')'))
-
+  select(date_time_gmt, water_temp_c, sensor_depth, date, transect, site, logger, serial)
 
 ## attach name to elevation data
 SN <- msmt.A.avgs$site_serial
@@ -206,7 +172,7 @@ for (i in 1:length(SN)) {
   el2 <- msmt.A.avgs3 %>%
     filter(site_serial == SN[[i]])
   
-  OUT2 <- tidal3.2 %>%
+  OUT2 <- tidal.01 %>%
     filter(site_serial == SN[[i]]) %>%
     mutate(
            wellcap_navd88 = el2$rtkcap_navd88,
