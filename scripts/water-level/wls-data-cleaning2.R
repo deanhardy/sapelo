a0cfbafe1c18c859355ee41efdfe76c180f5ca73
“Dean <deanhardy@gmail.com>
2025-08-14 15:11:25 +0000

    working on revising VE data now

--- scripts/water-level/wls-data-cleaning2.R
+++ scripts/water-level/wls-data-cleaning2.R
@@ -89,27 +89,24 @@ filz <- list.files(path = file.path(datadir, 'new-logger-data/hobo-sensor-depth'
                    pattern= '*.csv',
                    full.names = TRUE,
                    recursive = F)
-# 
-# ## test file
-# filz <- "/Users/dhardy/Dropbox/r_data/sapelo/water-level/new-logger-data/hobo/site06_1316_190522-191022.csv"
-# 
-# filz.ve <- list.files(path = file.path(datadir, 'new-logger-data/vanessen'),
-#                       pattern= '*.CSV',
-#                       full.names = TRUE,
-#                       recursive = TRUE) 
-# 
-# ## convert salinity Excel files to csv
-# sal <- list.files(path = file.path(datadir, 'new-logger-data/salinity'),
-#                   pattern= '*.xlsx',
-#                   full.names = TRUE,
-#                   recursive = TRUE) 
-# created <- mapply(convert, sal, gsub("xlsx", "csv", sal))
-# unlink(sal) # delete xlsx files
-# 
-# filz.psu <- list.files(path = file.path(datadir, 'new-logger-data/salinity'),
-#                        pattern= '*.csv',
-#                        full.names = TRUE,
-#                        recursive = TRUE) 
+
+filz.ve <- list.files(path = file.path(datadir, 'new-logger-data/vanessen'),
+                      pattern= '*.CSV',
+                      full.names = TRUE,
+                      recursive = TRUE)
+
+## convert salinity Excel files to csv
+sal <- list.files(path = file.path(datadir, 'new-logger-data/salinity'),
+                  pattern= '*.xlsx',
+                  full.names = TRUE,
+                  recursive = TRUE)
+created <- mapply(convert, sal, gsub("xlsx", "csv", sal))
+unlink(sal) # delete xlsx files
+
+filz.psu <- list.files(path = file.path(datadir, 'new-logger-data/salinity'),
+                       pattern= '*.csv',
+                       full.names = TRUE,
+                       recursive = TRUE)
 ## read test file
 # temp <- fread(paste0(datadir, 'new-logger-data/hobo-sensor-depth/t102_0000_181013-250426.csv'),
 #               select = c(2:5),
@@ -135,25 +132,6 @@ for(i in 1:length(filz)) {
       water_temp_c = as.numeric(water_temp_c),
       sensor_depth = as.numeric(sensor_depth),
       temp_max = max(water_temp_c)) %>%
-    # mutate(site = paste('Site', site, sep = '-')) %>%
-    # mutate(name = if_else(site == 'Site-02', 'Snagtree',
-    #                       if_else(site == 'Site-03', 'St. Lukes',
-    #                               if_else(site == 'Site-05', 'Graball',
-    #                                       if_else(site == 'Site-06', 'Dani Trap',
-    #                                               if_else(site == 'Site-07', 'Cactus Patch',
-    #                                                       if_else(site == 'Site-09', 'Mr. Tracy',
-    #                                                               if_else(site == 'Site-11', 'Library',
-    #                                                                       if_else(site == 'Site-12', 'Mr. Smith',
-    #                                                                               if_else(site == 'Site-13', 'Purple Ribbon',
-    #                                                                                       if_else(site == 'Site-14', 'Tidal Gate',
-    #                                                                                               if_else(site == 'Site-19', 'The Trunk',
-    #                                                                                                       if_else(site == 'Site-16', 'South Oakdale',
-    #                                                                                                               if_else(site == 'Site-18', 'NW Corner',
-    #                                                                                                                       if_else(site == 'Site-20', 'Walker',
-    #                                                                                                                               if_else(site == 'Site-23', 'Hillery',
-    #                                                                                                                                       if_else(site == 'Site-24', 'Johnson', 
-    #                                                                                                                                               if_else(site == 'Site-15', 'Oakdale', site)))))))))))))))))) %>%
-    # mutate(sitename = paste(site, name)) %>%
     select(!(abs_pres_psi))
   tidal <- rbind(OUT, tidal)
 }
@@ -162,7 +140,128 @@ for(i in 1:length(filz)) {
 tidal.01 <- tidal %>%
   mutate(logger = 'hobo',
          site_serial = paste0(site, ' (', serial, ')')) %>%
-  select(date_time_gmt, water_temp_c, sensor_depth, date, transect, site, logger, serial, site_serial)
+  select(date_time_gmt, water_temp_c, sensor_depth, date, transect, site, type, logger, serial, site_serial)
+
+## import & tidy van essen water level data
+## note water level C is in meters and indicates water level in reference to top of wellcap (negative numbers indicate below for VE data)
+tidal.ve <- NULL
+try(
+  for(i in 1:length(filz.ve)) {
+    OUT <- fread(filz.ve[i],
+                 skip = 51,
+                 select = c(1:3),
+                 col.names = c('date_time_gmt', 'water_level_C', 'water_temp_c'),
+                 stringsAsFactors = FALSE) %>%
+      slice(., burn:(n())) %>% ## removes first and last ## readings
+      # slice_head(n = 5) %>% ## removes first # rows
+      # slice_tail(n = 6) %>% ## removes last # rows
+      mutate(
+        date_time_gmt = as.POSIXct(date_time_gmt, format = '%Y/%m/%d %H:%M:%S', tz = 'GMT'),
+        date = as.Date(date_time_gmt, '%m/%d/%y', tz = 'GMT'),
+        site = str_sub(filz.ve[i], -26,-25),
+        serial = str_sub(filz.ve[i], -23,-19),
+        water_level_C = as.numeric(water_level_C)/1000 * -1) %>%
+      mutate(site = paste('Site', site, sep = '-')) %>%
+      mutate(type = ifelse(site %in% c('Site-13', 'Site-11', 'Site-09', 'Site-05', 'Site-19', 'Site-14', 'Site-15', 'Site-18', 'Site-20', 'Site-23', 'Site-24'), 'ditch',
+                           ifelse(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12', 'Site-16'), 'creek', site))) %>% ## ditches sites
+      # filter(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12')) %>% ## marsh sites
+      mutate(transect = ifelse(site %in% c('Site-06', 'Site-12', 'Site-19', 'Site-13', 'Site-18'), 'T1',
+                               ifelse(site %in% c('Site-02', 'Site-03', 'Site-05','Site-11', 'Site-23'), 'T3',
+                                      ifelse(site %in% c('Site-07', 'Site-09', 'Site-24'), 'T4',
+                                             ifelse(site %in% c('Site-16', 'Site-15'), "T5",
+                                                    ifelse(site %in% c('Site-20'), 'T2',
+                                                           if_else(site %in% c('Site-14'), 'T6', site))))))) %>%
+    mutate(site = if_else(site == 'Site-06', 'T1-01',
+                              if_else(site == 'Site-12', 'T1-02', 
+                                      if_else(site == 'Site-19', 'T1-03',
+                                              if_else(site == 'Site-13', 'T1-04',
+                                                      if_else(site == 'Site-18', 'T1-05',
+                                                              if_else(site == 'Site-20', 'T2-01', 
+                                                                      if_else(site == 'Site-02', 'T3-01',
+                                                                              if_else(site == 'Site-03', 'T3-02',
+                                                                                      if_else(site =='Site-05', 'T3-BR-01',
+                                                                                              if_else(site == 'Site-11', 'T3-03',
+                                                                                                      if_else(site == 'Site-23', 'T3-04',
+                                                                                                              if_else(site == 'Site-07', 'T4-01',
+                                                                                                                      if_else(site == 'Site-09', 'T4-BR-01',
+                                                                                                                              if_else(site == 'Site-24', 'T4-02',
+                                                                                                                                      if_else(site == 'Site-16', 'T5-01',
+                                                                                                                                              if_else(site == 'Site-15', 'T5-02', 
+                                                                                                                                                      if_else(site == 'Site-14', 'T6-01', site)))))))))))))))))) %>%
+      mutate(site_serial = paste0(site, ' (', serial, ')'))
+    tidal.ve <- rbind(OUT, tidal.ve)
+  }
+)
+
+## select relevant ve data columns
+tidal.ve2 <- tidal.ve %>%
+  mutate(logger = 'van essen') %>%
+  select(date_time_gmt, water_temp_c, water_level_C, date, transect, site, type, logger, serial, site_serial)
+
+## merge hobo and van essan data
+tidal1 <- rbind(tidal.01, tidal.ve2)
+
+## import & tidy van essen specific conductivity/salinity data
+## note water level C is in meters and indicates water level in reference to top of wellcap (negative numbers indicate below for VE data)
+tidal.psu <- NULL
+for(i in 1:length(filz.psu)) {
+  OUT <- fread(filz.psu[i],
+               select = c(2:8),
+               col.names = c('date_time_vartz', 'pressure', 'water_temp_c', 'conductivity', 'water_level_C', 'datum_reference', 'salinity'),
+               stringsAsFactors = FALSE) %>%
+    # slice(., 5:(n()-7)) %>% ## removes first and last ## readings
+    mutate(date_time_vartz = ymd_hms(date_time_vartz),
+           tz = str_sub(filz.psu[i], -7, -5),
+           site = str_sub(filz.psu[i], -30, -29),
+           water_temp_c = as.numeric(water_temp_c),
+           water_level_C = as.numeric(water_level_C)/1000 * -1,
+           serial = 'X0976') %>%
+    mutate(site = paste('Site', site, sep = '-')) %>%
+    mutate(site = ifelse(site %in% c('Site-13', 'Site-11', 'Site-09', 'Site-05', 'Site-19', 'Site-14', 'Site-15', 'Site-18', 'Site-20', 'Site-23', 'Site-24'), 'ditch',
+                         ifelse(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12', 'Site-16'), 'creek', site))) %>% ## ditches sites
+    # filter(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12')) %>% ## marsh sites
+    mutate(transect = ifelse(site %in% c('Site-06', 'Site-12', 'Site-19', 'Site-13', 'Site-18'), 'T1',
+                             ifelse(site %in% c('Site-02', 'Site-03', 'Site-05','Site-11', 'Site-23'), 'T3',
+                                    ifelse(site %in% c('Site-07', 'Site-09', 'Site-24'), 'T4',
+                                           ifelse(site %in% c('Site-16', 'Site-15'), "T5",
+                                                  ifelse(site %in% c('Site-20'), 'T2',
+                                                         if_else(site %in% c('Site-14'), 'T6', site))))))) %>%
+    mutate(site = if_else(site == 'Site-06', 'T1-01',
+                          if_else(site == 'Site-12', 'T1-02', 
+                                  if_else(site == 'Site-19', 'T1-03',
+                                          if_else(site == 'Site-13', 'T1-04',
+                                                  if_else(site == 'Site-18', 'T1-05',
+                                                          if_else(site == 'Site-20', 'T2-01', 
+                                                                  if_else(site == 'Site-02', 'T3-01',
+                                                                          if_else(site == 'Site-03', 'T3-02',
+                                                                                  if_else(site =='Site-05', 'T3-BR-01',
+                                                                                          if_else(site == 'Site-11', 'T3-03',
+                                                                                                  if_else(site == 'Site-23', 'T3-04',
+                                                                                                          if_else(site == 'Site-07', 'T4-01',
+                                                                                                                  if_else(site == 'Site-09', 'T4-BR-01',
+                                                                                                                          if_else(site == 'Site-24', 'T4-02',
+                                                                                                                                  if_else(site == 'Site-16', 'T5-01',
+                                                                                                                                          if_else(site == 'Site-15', 'T5-02', 
+                                                                                                                                                  if_else(site == 'Site-14', 'T6-01', site)))))))))))))))))) %>%
+    mutate(site_serial = paste0(site, ' (', serial, ')'))
+
+  tidal.psu <- rbind(OUT, tidal.psu)
+}
+
+options(scipen=999)
+
+## clean up salinity data
+tidal.psu2 <- tidal.psu %>%
+  mutate(date_time_gmt = as.POSIXct(
+    if_else(tz == 'est', date_time_vartz + hours(5),
+            if_else(tz == 'edt', date_time_vartz + hours(4), date_time_vartz)
+    ))) %>%
+  select(date_time_gmt, site, salinity) %>%
+  filter(salinity < 50 | is.na(salinity))
+
+## merge salinity data to water level data 
+tidal1.1 <- full_join(tidal1, tidal.psu2, by = c('site', 'date_time_gmt')) %>%
+  select(!(site.y))
 
 ## attach name to elevation data
 SN <- msmt.A.avgs$site_serial
