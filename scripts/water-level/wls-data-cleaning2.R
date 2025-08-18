9881aea9f12ce8e70323451b007a327412ecb475
“Dean <deanhardy@gmail.com>
2025-08-18 01:48:50 +0000

    working toward sreamlining new data cleaning process with depth data

--- scripts/water-level/wls-data-cleaning2.R
+++ scripts/water-level/wls-data-cleaning2.R
@@ -107,6 +107,7 @@ filz.psu <- list.files(path = file.path(datadir, 'new-logger-data/salinity'),
                        pattern= '*.csv',
                        full.names = TRUE,
                        recursive = TRUE)
+
 ## read test file
 # temp <- fread(paste0(datadir, 'new-logger-data/hobo-sensor-depth/t102_0000_181013-250426.csv'),
 #               select = c(2:5),
@@ -146,26 +147,27 @@ tidal.01 <- tidal %>%
 tidal.ve <- NULL
 try(
   for(i in 1:length(filz.ve)) {
-    OUT <- fread(filz.ve[i],
+    OUT <- fread(filz.ve[2],
                  skip = 63,
                  select = c(1:3),
                  col.names = c('date_time_gmt', 'sensor_depth', 'water_temp_c'),
                  stringsAsFactors = FALSE) %>%
       slice(., burn:(n())) %>% ## removes first and last ## readings
       mutate(
-        date_time_gmt = as.POSIXct(date_time_gmt, format = '%m/%d/%Y %H:%M', tz = 'GMT'),
+        date_time_gmt = as.POSIXct(date_time_gmt, format = '%m/%d/%y %H:%M', tz = 'GMT'),
         date = as.Date(date_time_gmt, '%m/%d/%y', tz = 'GMT'),
         transect = toupper(str_sub(filz.ve[i], -28,-27)),
-        site = toupper(paste0(str_sub(filz.ve[i], -28,-27), '-', str_sub(filz[i], -26,-25))),
+        site = toupper(paste0(str_sub(filz.ve[i], -28,-27), '-', str_sub(filz.ve[i], -26,-25))),
         serial = str_sub(filz.ve[i], -23,-19),
         sensor_depth = round(as.numeric(sensor_depth)/1000,3)) %>%                                                                                                                                   
-      mutate(site_serial = paste0(site, ' (', serial, ')'))
+      mutate(site_serial = paste0(site, ' (', serial, ')')) %>%
+      filter(sensor_depth != 'NA')
 
     tidal.ve <- rbind(OUT, tidal.ve)
   }
 )
 
-## select relevant ve data columns
+ ## select relevant ve data columns
 tidal.ve2 <- tidal.ve %>%
   mutate(logger = 'van essen') %>%
   select(date_time_gmt, water_temp_c, sensor_depth, date, transect, site, logger, serial, site_serial)
@@ -184,38 +186,13 @@ for(i in 1:length(filz.psu)) {
     # slice(., 5:(n()-7)) %>% ## removes first and last ## readings
     mutate(date_time_vartz = ymd_hms(date_time_vartz),
            tz = str_sub(filz.psu[i], -7, -5),
-           site = str_sub(filz.psu[i], -30, -29),
+           transect = toupper(str_sub(filz.psu[i], -32,-31)),
+           site = toupper(paste0(str_sub(filz.psu[i], -32,-31), '-', 
+                                 str_sub(filz.psu[i], -30,-29))),
+           serial = str_sub(filz.psu[i], -27,-23),          
            water_temp_c = as.numeric(water_temp_c),
            water_level_C = as.numeric(water_level_C)/1000 * -1,
-           serial = 'X0976') %>%
-    mutate(site = paste('Site', site, sep = '-')) %>%
-    mutate(site = ifelse(site %in% c('Site-13', 'Site-11', 'Site-09', 'Site-05', 'Site-19', 'Site-14', 'Site-15', 'Site-18', 'Site-20', 'Site-23', 'Site-24'), 'ditch',
-                         ifelse(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12', 'Site-16'), 'creek', site))) %>% ## ditches sites
-    # filter(site %in% c('Site-02', 'Site-03', 'Site-07', 'Site-06', 'Site-12')) %>% ## marsh sites
-    mutate(transect = ifelse(site %in% c('Site-06', 'Site-12', 'Site-19', 'Site-13', 'Site-18'), 'T1',
-                             ifelse(site %in% c('Site-02', 'Site-03', 'Site-05','Site-11', 'Site-23'), 'T3',
-                                    ifelse(site %in% c('Site-07', 'Site-09', 'Site-24'), 'T4',
-                                           ifelse(site %in% c('Site-16', 'Site-15'), "T5",
-                                                  ifelse(site %in% c('Site-20'), 'T2',
-                                                         if_else(site %in% c('Site-14'), 'T6', site))))))) %>%
-    mutate(site = if_else(site == 'Site-06', 'T1-01',
-                          if_else(site == 'Site-12', 'T1-02', 
-                                  if_else(site == 'Site-19', 'T1-03',
-                                          if_else(site == 'Site-13', 'T1-04',
-                                                  if_else(site == 'Site-18', 'T1-05',
-                                                          if_else(site == 'Site-20', 'T2-01', 
-                                                                  if_else(site == 'Site-02', 'T3-01',
-                                                                          if_else(site == 'Site-03', 'T3-02',
-                                                                                  if_else(site =='Site-05', 'T3-BR-01',
-                                                                                          if_else(site == 'Site-11', 'T3-03',
-                                                                                                  if_else(site == 'Site-23', 'T3-04',
-                                                                                                          if_else(site == 'Site-07', 'T4-01',
-                                                                                                                  if_else(site == 'Site-09', 'T4-BR-01',
-                                                                                                                          if_else(site == 'Site-24', 'T4-02',
-                                                                                                                                  if_else(site == 'Site-16', 'T5-01',
-                                                                                                                                          if_else(site == 'Site-15', 'T5-02', 
-                                                                                                                                                  if_else(site == 'Site-14', 'T6-01', site)))))))))))))))))) %>%
-    mutate(site_serial = paste0(site, ' (', serial, ')'))
+           serial = 'X0976')
 
   tidal.psu <- rbind(OUT, tidal.psu)
 }
@@ -232,8 +209,9 @@ tidal.psu2 <- tidal.psu %>%
   filter(salinity < 50 | is.na(salinity))
 
 ## merge salinity data to water level data 
-tidal1.1 <- full_join(tidal1, tidal.psu2, by = c('site', 'date_time_gmt')) %>%
-  select(!(site.y))
+tidal1.1 <- full_join(tidal1, tidal.psu2, by = c('site', 'date_time_gmt')) 
+# %>%
+#   select(!(site.y))
 
 ## attach name to elevation data
 SN <- msmt.A.avgs$site_serial
@@ -247,7 +225,7 @@ for (i in 1:length(SN)) {
   el2 <- msmt.A.avgs3 %>%
     filter(site_serial == SN[[i]])
   
-  OUT2 <- tidal.01 %>%
+  OUT2 <- tidal1 %>%
     filter(site_serial == SN[[i]]) %>%
     mutate(
            wellcap_navd88 = el2$rtkcap_navd88,
